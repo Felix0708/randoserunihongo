c7bcb0fc32ec216b9c7c25553ffb8f393230e5f1
Felix0708 <chuls0708@gmail.com>
2023-11-11 17:34:08 +0900

    BASE

---
+++ randoserunihongo_back/src/main/java/com/example/randoserunihongo_back/RandoserunihongoBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.randoserunihongo_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandoserunihongoBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RandoserunihongoBackApplication.class, args);
+    }
+
+}
---
+++ randoserunihongo_back/src/test/java/com/example/randoserunihongo_back/RandoserunihongoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.randoserunihongo_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandoserunihongoBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
