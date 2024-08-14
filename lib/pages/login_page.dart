42026c18e585e98a497738347dca1449516eaadf
Felix0708 <chuls0708@gmail.com>
2024-08-15 00:54:03 +0900

    google login

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -2,6 +2,8 @@ import 'package:randosel_main_1/colors/login_button_color.dart';
 import 'package:flutter/material.dart';
 
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:randosel_main_1/main.dart';
 
 class AuthScreen extends StatelessWidget {
 
@@ -40,15 +42,33 @@ class AuthScreen extends StatelessWidget {
       );
     }
     onGoogleLoginPress(BuildContext context) async {
-      GoogleSignIn googleSignIn = GoogleSignIn(
+      GoogleSignIn googleSignIn = GoogleSignIn(     
         scopes: [
           'email',
         ],
+
       );
 
       try {
         GoogleSignInAccount? account = await googleSignIn.signIn();
+        print("acount: ");
         print(account);
+
+        final GoogleSignInAuthentication? googleAuth = await account?.authentication;
+
+        final credential = GoogleAuthProvider.credential(
+          accessToken: googleAuth?.accessToken,
+          idToken: googleAuth?.idToken,
+        );
+
+        await FirebaseAuth.instance.signInWithCredential(credential);
+
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (_) => MainPage()
+          ),
+        );
+
       } catch(error) {
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(content: Text('failed to login'))
